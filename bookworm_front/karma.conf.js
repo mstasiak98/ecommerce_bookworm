bae4fddf322777283a2dc1a3f9c6e9fee169cdf3
mstasiak98 <mikistasiak98@gmail.com>
2023-01-05 16:31:14 +0100

    init frontend - install tailwind, primeng and eslint

--- bookworm_front/karma.conf.js
+++ bookworm_front/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bookworm-frontend'),
+      dir: require('path').join(__dirname, './coverage/bookworm_front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- bookworm_front/src/app/app.component.spec.ts
+++ bookworm_front/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bookworm-frontend'`, () => {
+  it(`should have as title 'bookworm_front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bookworm-frontend');
+    expect(app.title).toEqual('bookworm_front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('bookworm-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('bookworm_front app is running!');
   });
 });
